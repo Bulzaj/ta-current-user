7dd29b6353dec48efc3826e7d0ccecc8cf5af092
bulzaj <ernestsado94@gmail.com>
2020-08-24 17:50:07 +0200

    first commit

---
+++ src/main/java/com/talkactive/tacurrentuser/TaCurrentUserApplication.java
@@ -0,0 +1,13 @@
+package com.talkactive.tacurrentuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaCurrentUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaCurrentUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talkactive/tacurrentuser/TaCurrentUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkactive.tacurrentuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaCurrentUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
